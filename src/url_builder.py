5ea0e021ea18e93a7423fb5bcacd9e3bc2b8a283
SaulLu <lucilesaul.com@gmail.com>
2020-05-26 16:01:58 +0200

    add URLBuilder class

--- src/url_builder.py
+++ src/url_builder.py
@@ -7,6 +7,11 @@ from src.graphquery import GraphQuery
 
 class URLBuilder:
     """class that allows to build the url of a query that will be made for an alias of a FHIR resource to a FHIR api
+
+    Attributes:
+        fhir_api_url: The Service Base URL (e.g. http://hapi.fhir.org/baseR4/)
+        main_resource_alias: alias given to a set of fhir resources of a certain type which are the subject of the api query
+        search_query_url: url which will make it possible to recover the resources of the type of main_alias respecting as well as possible the conditions where on itself and on its neighbors.
     """
     def __init__(
         self,
@@ -14,35 +19,42 @@ class URLBuilder:
         query_graph: type(GraphQuery),
         main_resource_alias: str
     ) -> None:
+        """
+        Arguments:
+            fhir_api_url {str} -- The Service Base URL (e.g. http://hapi.fhir.org/baseR4/)
+            query_graph {type(GraphQuery)} -- instance of a GraphQuery object that gives a graphical representation of the global query
+            main_resource_alias {str} -- alias given to a set of fhir resources of a certain type
+        """
         self.fhir_api_url = fhir_api_url
-        self.query_graph = query_graph
         self.main_resource_alias = main_resource_alias
-
+        self._query_graph = query_graph
+        
         self._url_params = None
         self._get_url_params()
         self.search_query_url = self._compute_url()
 
     def _compute_url(self) -> str:
-        """Generates the API request url satisfying the conditions from,
-        join, where and select
+        """Generates the url which will make it possible to recover the resources of the type of main_alias respecting as well as possible the conditions where on itself and on its neighbors.
 
         Returns:
             str -- corresponding API request url
         """
         # to do verify self.fhir_api_url finish by '/'
         search_query_url = (
-            f"{self.fhir_api_url}{self.query_graph.resources_alias_info[self.main_resource_alias]['resource_type']}?"
+            f"{self.fhir_api_url}{self._query_graph.resources_alias_info[self.main_resource_alias]['resource_type']}?"
             f"{self._url_params or ''}&_format=json"
         )
         return search_query_url
 
     def _get_url_params(self):
-        for ressource_alias in self.query_graph.resources_alias_info.keys():
+        """retrieves the portions of the url that specify search parameters
+        """
+        for ressource_alias in self._query_graph.resources_alias_info.keys():
             url_temp = None
             to_resource, reliable = self._chained_params(ressource_alias)
 
             if reliable:
-                for search_param, values in self.query_graph.resources_alias_info[ressource_alias][
+                for search_param, values in self._query_graph.resources_alias_info[ressource_alias][
                     "search_parameters"].items():
                     # add assert search_param in CapabilityStatement
                     value = f"{values['prefix'] or ''}{values['value']}"
@@ -55,21 +67,30 @@ class URLBuilder:
                         )
                 # logging.info(f"url_params: {self._url_params}")
 
-    def _chained_params(self, ressource_alias):
+    # To change because it's useless to go through dijstra for the moment knowing that we only do chain parameters of length 1.
+    def _chained_params(self, ressource_alias:str) -> tuple:
+        """gives the prefix (in the first element of the output tuple) to make a chained parameter from the main resource to the resource given as argument. If the resource given as argument is not a neighbor of the main resource, the second element of the output tuple is set to false
+
+        Arguments:
+            ressource_alias {str} -- alias of a resource
+
+        Returns:
+            tuple -- (prefix, boolean)
+        """
         to_resource = None
         reliable = True
         # Construction of the path from the main resource to the
         # resource on which the parameter(s) will be applied
         if ressource_alias != self.main_resource_alias:
             internal_path = nx.shortest_path(
-                self.query_graph.resources_alias_graph,
+                self._query_graph.resources_alias_graph,
                 source=self.main_resource_alias,
                 target=ressource_alias,
             )
             # because we are not sure about chaining
             if len(internal_path) <= 2:
                 for ind in range(len(internal_path) - 1):
-                    edge = self.query_graph.resources_alias_graph.edges[
+                    edge = self._query_graph.resources_alias_graph.edges[
                         internal_path[ind], internal_path[ind + 1]
                     ]
                     #logging.info(f"edge:{edge}")
