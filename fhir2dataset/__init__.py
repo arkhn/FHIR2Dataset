98f7f6382e3869e4db134138e25b32f2ca9e80d4
SaulLu <55560583+SaulLu@users.noreply.github.com>
2020-08-06 15:45:42 +0200

    Add parser (#50)
    * git add parser
    
    * fix test 5
    
    * update tutorial in html format
    
    * update tutorials
    
    * add readeable html tutorial
    
    * remove print
    
    * add flexibility for string query
    
    * clean create config
    
    * be more specific with delimiter words
    
    * config join
    
    * .replace(" ", "") become .strip()
    
    * delete self.__create_mask_multiple_separators
    
    * case-insensitive on reserved SQL words
    
    * change join_rule.strip() == "id"
    
    * create a dedicated function which handles the spaces with dedicated key words
    
    * change string prefix
    
    * change default argument keep_separators

--- fhir2dataset/__init__.py
+++ fhir2dataset/__init__.py
@@ -1,3 +1,4 @@
 from fhir2dataset.query import Query  # noqa
 from fhir2dataset.fhirrules_getter import FHIRRules  # noqa
+from fhir2dataset.parser import FHIR2DatasetParser  # noqa
 from fhir2dataset.timer import timing  # noqa
