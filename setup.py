be69ff1eb6d1422413a6c8c2c74c20a7979923fa
SaulLu <lucilesaul.com@gmail.com>
2020-08-12 15:46:42 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     name="fhir2dataset",
     packages=find_packages(),
     include_package_data=True,
-    version="0.0.10",
+    version="0.1.0",
     license="Apache License 2.0",
     description="Transform FHIR to Dataset",
     long_description=long_description,
@@ -33,7 +33,7 @@ setup(
     author="Lucile Saulnier",
     author_email="contact@arkhn.com",
     url="https://github.com/arkhn/FHIR2Dataset",
-    keywords=["arkhn", "medical", "fhir"],
+    keywords=["arkhn", "medical", "fhir", "FHIR", "Dataset", "API"],
     install_requires=requirements,
     classifiers=[
         "Development Status :: 3 - Alpha",
