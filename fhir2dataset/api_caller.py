fe331cc7d3e26f3956e56615f6ea803c194495f9
SaulLu <lucilesaul.com@gmail.com>
2020-08-26 16:29:12 +0200

    remove debugging print

--- fhir2dataset/api_caller.py
+++ fhir2dataset/api_caller.py
@@ -4,7 +4,6 @@ import logging
 from itertools import product
 from typing import Type
 from dataclasses import asdict
-from dacite import from_dict
 
 from fhir2dataset.timer import timing
 from fhir2dataset.fhirpath import fhirpath_processus_tree
@@ -165,7 +164,6 @@ class ApiGetter(CallApi):
     def _get_data(self, response: Type[Response]):
         """retrieves the necessary information from the json instance of a resource and stores it in the data attribute
         """  # noqa
-        elements_empty = asdict(self.elements)
         if not response.results:
             columns = [element.col_name for element in self.elements.elements]
             df = pd.DataFrame(columns=columns)
@@ -174,32 +172,15 @@ class ApiGetter(CallApi):
                 "the current page doesnt have an entry keyword, therefore an empty df is created"
             )
         else:
-            print()
-            # print("elements")
-            # print(self.elements.elements)
-
             for json_resource in response.results:
                 resource = json_resource["resource"]
                 elements = self.elements.elements.copy()
-                # print("forest_dict")
-                # print(self.elements.forest_dict)
-                # print("resource")
-                # print(resource)
 
                 data_array = fhirpath_processus_tree(self.elements.forest_dict, resource)
-                # print("data_array")
-                # print(data_array)
                 for element_value, element in zip(data_array, elements):
                     element.value = element_value
-                # print("elements")
-                # print(elements)
                 df = self._flatten_item_results(elements)
                 self.df = pd.concat([self.df, df])
-                print(self.df)
-            # for data_dict in data_dicts:
-            #     elements = from_dict(data_class=Elements, data=data_dict)
-            #     df = self._flatten_item_results(elements)
-            #     self.df = pd.concat([self.df, df])
         return response.next_url
 
     @timing
